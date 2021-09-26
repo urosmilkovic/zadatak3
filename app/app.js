334cf181819683402817a78e4d93a73dfa1f2af5
Uros Milkovic <urosmilkovic@gmail.com>
2021-09-26 11:32:56 +0200

    tablet-responsive

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-$('onclick')
+$(document).ready(function () {
+    $('.button-req').click(function() {
+        $('.error').text("Oops! Please check your email");
+    });
+});
+
+// $(document).ready(function () {
+//     $('.button-req').click(function() {
+//         $('.error').toggleClass("fix-error");
+//     });
+// });
